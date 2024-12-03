b53780c788e6ea36325e259b8c518f36c39a8de1
George Dong <georgedong51@gmail.com>
2024-12-03 17:50:16 -0500

    Implemented auth logic using Firebase Bearer Token

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from config import Config
+import firebase_admin
+from firebase_admin import credentials
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Initialize Firebase Admin SDK to verify token
+cred = credentials.Certificate('app/serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,32 @@
 from app import app
+from app.auth import firebase_auth_required
+from flask import jsonify, g
 
-@app.route('/')
-@app.route('/api')
-def index():
-    user = {
-        "id": "0",
-        "firebase_uid": "1234567890",
-        "name" : "Mock User"
+posts = [
+    {
+        "id" : "0",
+        "user_id" : "0", 
+        "created_at" : "Hello, World!", 
+        "post_content" : "This is a test post.",
+        "sadness_amt" : 0.5,
+        "fear_amt" : 0.5,
+        "joy_amt" : 0.5,
+        "anger_amt" : 0.5
     }
+]
 
-    posts = [
-        {
-            "id" : "0",
-            "user_id" : "0", 
-            "created_at" : "Hello, World!", 
-            "post_content" : "This is a test post.",
-            "sadness_amt" : 0.5,
-            "fear_amt" : 0.5,
-            "joy_amt" : 0.5,
-            "anger_amt" : 0.5
-        }
-    ]
+@app.route('/api')
+@firebase_auth_required
+def index():
+    """
+    Endpoint to test authentication.
+    """
+    return jsonify({'message': 'Authentication successful!'}), 200
 
-    return posts
-    
+@app.route('/api/posts')
+@firebase_auth_required
+def get_posts():
+    """
+    Endpoint to retrieve user's posts.
+    """
+    return jsonify({"posts" : posts}), 200
