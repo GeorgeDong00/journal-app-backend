2ffed67f80daf7a9cddf1c273fc9772503db4199
George Dong <georgedong51@gmail.com>
2024-12-06 03:54:45 -0500

    Create Celery tasks registry for Celery worker instance

---
+++ app/celery_tasks.py
@@ -0,0 +1,24 @@
+from app.tasks_logic import generate_weekly_advice_for_user
+from app.models import User
+from app.extensions import db
+
+def register_tasks(celery):
+    """
+    Defines and register multiple Celery tasks with the Celery instance.
+
+    Parameters:
+        celery (Celery): Instance to which tasks will be registered to.
+    """
+    
+    @celery.task(name='generate_all_users_weekly_advice')
+    def generate_all_users_weekly_advice():
+        """
+        Defines a Celery task that generates personalized weekly advice for each unqiue user.
+
+        Returns:
+            str: Message indicating the completion status of the task.
+        """
+        users = User.query.all()
+        for user in users:
+            generate_weekly_advice_for_user(user)
+        return "Completed Celery Task: Generated all users' weekly advice."
