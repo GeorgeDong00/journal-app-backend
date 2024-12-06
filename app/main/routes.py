b373dfbd2ea8663cbca97380a8a47fc10088af36
Abrar Amin <abraramin.school@gmail.com>
2024-12-06 00:36:18 -0500

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,14 +1,39 @@
 from flask import g, jsonify, request
 from marshmallow import ValidationError
 from app.extensions import db
+import app.models.post as post
+import app.models.weekly_advice as weekly_advice
+import datetime
 from app.auth import firebase_auth_required
+
 from app.models import (
     User,
     Post, 
     PostSchema
 )
 from . import main_bp
+from transformers import pipeline 
 
+emotion_analyzer = pipeline(
+    "text-classification", 
+    model="bhadresh-savani/bert-base-uncased-emotion", 
+    return_all_scores=True  # Return scores for all emotions
+)
+
+
+def serializePost(post):
+    post_data = {}
+    post_data["id"] = post.id
+    post_data["user_id"] = post.user_id
+    post_data["content"] = post.content
+    post_data["dominant_emotion"] = post.dominant_emotion
+    post_data["dominant_emotion_value"] = post.dominant_emotion_value
+    post_data["created_at"] = post.created_at
+
+    return post_data
+
+def returnLastSunday(date):
+    return date - datetime.timedelta(days=date.weekday()+1)
 
 def get_or_create_user(firebase_uid):
     """
@@ -33,15 +58,20 @@ def index():
 
 
 @main_bp.route("/api/posts", methods=["POST"])
-@firebase_auth_required
+#@firebase_auth_required
 def create_post():
     """
     Endpoint to create a new post for the authenticated user.
     """
-    firebase_uid = g.user['uid']
-    # firebase_uid = "test_user"
+    #firebase_uid = g.user['uid']
+    firebase_uid = "test_user"
     user = get_or_create_user(firebase_uid)
     data = request.get_json()
+    input = emotion_analyzer(data['content'])
+    print(input)
+    #data['dominant_emotion'] = emotion_label[0]['label']
+    #data['dominant_emotion_value'] = emotion_label[0]['score']
+  
 
     # Validate request data
     post_schema = PostSchema()
@@ -52,7 +82,6 @@ def create_post():
 
     new_post = post_data
     new_post.user_id = user.id
-
     try:
         db.session.add(new_post)
         db.session.commit()
@@ -62,3 +91,38 @@ def create_post():
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": "Failed to create post.", "message": str(e)}), 500
+
+
+  
+
+
+
+@main_bp.route("/api/posts/", methods=["GET"])
+@firebase_auth_required
+def get_users_posts():
+    """
+    Endpoint to create a new post for the authenticated user.
+    """
+    firebase_uid1 = g.user['uid']
+    print(g.user['email'])
+    #firebase_uid = "test_user"
+    #user = get_or_create_user(firebase_uid)
+    user = db.session.query(User).filter_by(firebase_uid = firebase_uid1).first()
+
+    res = db.session.query(User).all()
+    for amo in res:
+        print(amo.firebase_uid)
+
+    posts = user.posts
+
+    #print(returnLastSunday(datetime.datetime(2023, 12, 30, 12, 0)))
+
+    #userSummaries = db.session.query(weekly_advice.WeeklyAdvice).filter_by(user_id = users_id)
+   # print(userSummaries)    
+    #result = db.session.execute(userSummaries)
+
+    all_posts = []
+    for post in posts:
+        all_posts.append(serializePost(post))
+
+    return jsonify({"message": "Post created successfully.", "posts": all_posts}),200
