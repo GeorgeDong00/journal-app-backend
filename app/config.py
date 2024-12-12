60100be15b615b067e9ff8175fe06016f7d5c869
George Dong <georgedong51@gmail.com>
2024-12-12 02:53:43 -0500

    Transition database to PostgreSQL from SQLite

--- app/config.py
+++ app/config.py
@@ -8,14 +8,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     # Secret key for CSRF protection
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+
+    # PostgreSQL Database
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Celery Configuration
-    BROKER_URL = os.environ.get("BROKER_URL") or "redis://localhost:6379/0"
-    RESULT_BACKEND = os.environ.get("RESULT_BACKEND") or "redis://localhost:6379/0"
+    BROKER_URL = os.environ.get("BROKER_URL")
+    RESULT_BACKEND = os.environ.get("RESULT_BACKEND")
 
     CELERY_BEAT_SCHEDULE = {
         "weekly-advice-generation": {
